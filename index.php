98bda1f16453f3a9f7be9ca78ce2585c9a01da83
idbaali <ikebaalidavid@live.de>
2023-06-08 17:34:23 +0200

    AM-Demarage projet

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+session_start();
+
+include("allInclude.php");
+
+
+$page = isset($_GET['page']) ? $_GET['page'] : 'home';
+
+switch ($page) {
+
+        /* HOME */
+    case 'home':
+       
+        include 'structures/front/pages/home.php';
+        break;
+        /* /HOME */
+
+
+
+
+        /* 404 NOT FOUND */
+    default:
+        header("HTTP/1.1 404 Not Found");
+        include 'structures/front/pages/page-404.php';
+        break;
+        /* /404 NOT FOUND */
+}
